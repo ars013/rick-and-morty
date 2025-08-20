222abe0e91a86deb6d8919170be1f8ffa8a37c44
Arslan Assankulov <arslan.assankulov@gmail.com>
2025-08-20 11:04:03 +0500

    Initial commit

---
+++ rick-and-morty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rick-and-morty
+//
+//  Created by Asankulov Arslan on 20.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
