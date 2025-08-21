9440760c6d82fae9701c6356c62004c97e7c93d4
Arslan Assankulov <arslan.assankulov@gmail.com>
2025-08-21 15:17:15 +0500

    Add check button and retrieve the word that user has entered

--- rick-and-morty/ViewController.swift
+++ rick-and-morty/ViewController.swift
@@ -19,6 +19,12 @@ class ViewController: UIViewController {
         tf.backgroundColor = .white
         return tf
     }()
+    
+    private let checkButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Get info about a cartoon character", for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +37,22 @@ class ViewController: UIViewController {
             make.center.equalToSuperview()
             make.leading.trailing.equalToSuperview().inset(20)
             make.height.equalTo(40)
+        }
+        
+        view.addSubview(checkButton)
+        checkButton.snp.makeConstraints { make in
+            make.top.equalTo(textField.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
+        }
+        
+        checkButton.addTarget(self, action: #selector(getTextFromField), for: .touchUpInside)
+    }
+    
+    @objc func getTextFromField() {
+        if let text = textField.text, !text.isEmpty {
+            print("User entered: \(text)")
+        } else {
+            print("Text field is empty")
         }
     }
 }
